911bc528260d64ca5abdc6e11fe7b6801942914c
Maksim <malcewdeveloper@yandex.ru>
2024-02-26 17:51:02 +0300

    feat: adds a Line component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as Stack } from './Stack';
 export { default as AboutMe } from './AboutMe';
+export { default as Line } from './Line';
