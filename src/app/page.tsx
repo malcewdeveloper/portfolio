5c42027047079fe43fe782a12adf191f6df32354
Maksim <malcewdeveloper@yandex.ru>
2024-03-02 22:05:40 +0300

    refactor: changes layout home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { Header } from "@/components";
+import { Header, Stack, AboutMe, ProjectMenu } from "@/components";
 
-export default function Home() {
+
+export default function HomePage() {
   return (
-    <React.Fragment>
+    <div className="h-[calc(100vh+50px)]">
       <Header />
-    </React.Fragment>
+      <main>
+        <section className="mt-[180px] max-w-7xl mx-auto flex items-center justify-between">
+          <div>
+            <Stack stacks={ ['React', 'UX/UI', 'Test'] }/>
+            <AboutMe me={ [{name: 'Maksim', city: 'Moscow', age: 10, profession: 'Frontend', job: [{name: 'Anywhere', period: [2020, 2020]}], projects: [{name: 'Name', link: 'projects/1', category: ['Frontend'], imageUrl: '/sooos.png'}], contacts: [{name: 'Telegram', url: '/'}] }] } />
+          </div>
+          <div className="group/text -rotate-90 text-center text-2xl justify-end cursor-pointer">
+            <div className="text-[#6A7273]">
+              Не смотри сюда
+            </div>
+            <div className="text-[#2F3A3B] invisible group-hover/text:visible">
+              Ну зачем ты навел сюда <br></br>теперь придется нанять
+            </div>
+          </div>
+        </section>
+        <ProjectMenu projects={ [{name: 'Testing', link: '/projects/1', category: ['Frontend'], imageUrl: '/sooos.png'}] } />
+      </main>
+    </div>
   );
 }
