2adadc386caa5f2de8e238ba72cf50a94b13b19e
Maksim <malcewdeveloper@yandex.ru>
2024-03-15 11:42:25 +0300

    refactor: changes props and display tag

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,20 +6,25 @@ import classes from './Card.module.scss';
 
 interface ICardProps {
     name?: string;
-    imageUrl: string;
+    previewImageUrl: string;
     url: string;
-    tags?: string[];
+    tag: string;
 }
 
 const Card: React.FC<ICardProps> = (props) => {
-    const { name, imageUrl, url, tags } = props;
+    const { 
+        name, 
+        previewImageUrl, 
+        url, 
+        tag 
+    } = props;
 
     return (
         <div className={ classes.root }>
             <Link className={ classes.link } href={ url }>
-                <Image className={ classes.image } src={ imageUrl } width={ 400 } height={ 550 } alt="This is preview image" />
+                <Image className={ classes.image } src={ previewImageUrl } width={ 400 } height={ 550 } alt="This is preview image" />
                 <div className={ classes.description }>
-                    { tags && tags.map(tag => <Tag key={ tag }>{ tag }</Tag>) }
+                    { tag && <Tag>{ tag }</Tag> }
                     <div className={ classes.name }>{ name }</div>
                 </div>
             </Link>
