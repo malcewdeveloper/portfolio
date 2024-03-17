71f19bbbe797fbf5cc39126d9fe538b586c54902
Maksim <malcewdeveloper@yandex.ru>
2024-03-17 18:17:52 +0300

    refactor: changes fake data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
 import { Header, Stack, AboutMe, ProjectMenu } from "@/components";
+import { fetchProjects, fetchUsers } from "./lib/data";
 
 
-export default function HomePage() {
-  return (
-    <div className="h-[calc(100vh+50px)]">
-      <Header />
-      <main>
-        <section className="mt-[180px] max-w-7xl mx-auto flex items-center justify-between">
-          <div>
-            <Stack stacks={ ['React', 'UX/UI', 'Test'] }/>
-            <AboutMe me={ [{name: 'Maksim', city: 'Moscow', age: 10, profession: 'Frontend', job: [{name: 'Anywhere', period: [2020, 2020]}], projects: [{name: 'Name', link: 'projects/1', category: ['Frontend'], imageUrl: '/sooos.png'}], contacts: [{name: 'Telegram', url: '/'}] }] } />
-          </div>
-          <div className="group/text -rotate-90 text-center text-2xl justify-end cursor-pointer">
-            <div className="text-[#6A7273]">
-              Не смотри сюда
-            </div>
-            <div className="text-[#2F3A3B] invisible group-hover/text:visible">
-              Ну зачем ты навел сюда <br></br>теперь придется нанять
-            </div>
-          </div>
-        </section>
-        <ProjectMenu projects={ [{name: 'Testing', link: '/projects/1', category: ['Frontend'], imageUrl: '/sooos.png'}] } />
-      </main>
-    </div>
-  );
+export default async function HomePage() {
+    const [projects, users] = await Promise.all([fetchProjects(), fetchUsers()]);
+
+
+    return (
+        <div className="h-[calc(100vh+50px)]">
+            <Header />
+            <main>
+                <section className="mt-[180px] max-w-7xl mx-auto flex items-center justify-between">
+                <div>
+                    <Stack stacks={ users[0].stack }/>
+                    <AboutMe me={ users } />
+                </div>
+                <div className="group/text -rotate-90 text-center text-2xl justify-end cursor-pointer">
+                    <div className="text-[#6A7273]">
+                    Не смотри сюда
+                    </div>
+                    <div className="text-[#2F3A3B] invisible group-hover/text:visible">
+                    Ну зачем ты навел сюда <br></br>теперь придется нанять
+                    </div>
+                </div>
+                </section>
+                <ProjectMenu projects={ projects } />
+            </main>
+        </div>
+    );
 }
