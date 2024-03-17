6ffb83a6641c4ebe2b3a6cb710974f6f9590f3d2
Maksim <malcewdeveloper@yandex.ru>
2024-03-17 17:56:22 +0300

    refactor: changes data properties from me

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -3,6 +3,7 @@ import { IMe } from "@/interfaces";
 import { Line } from "@/components";
 import Link from "next/link";
 import classes from './AboutMe.module.scss'
+import { convertDateToAge } from "@/utils";
 
 
 interface IAboutMeProps {
@@ -19,21 +20,21 @@ const AboutMe: React.FC<IAboutMeProps> = (props) => {
                 <React.Fragment key={ me.name }>
                     <Line tag="name" number={ ++number }>{ me.name }</Line>
                     <Line tag="city" number={ ++number }>{ me.city }</Line>
-                    <Line tag="age" number={ ++number }>{ `${me.age} года` }</Line>
+                    <Line tag="age" number={ ++number }>{ convertDateToAge(me.birthday) }</Line>
                     <Line tag="profession" number={ ++number }>{ me.profession }</Line>
                     <Line number={ ++number } />
-                    {me.job.map(job => (
+                    {me.jobs.map(job => (
                         <Line key={ job.name } tag={`job ${job.period[0]}-${job.period[1]}`} number={ ++number }>{ job.name }</Line>
                     ))}
-                    {me.projects.map(project => (
+                    {me.projects_data.map(project => (
                         <Line key={ project.name } tag="my project" number={ ++number }>
-                            { <Link className="text-[#FEDB31]" href={ project.link }>{ project.name }</Link> }
+                            { <Link className="text-[#FEDB31]" href={`/projects/${ project.id }`}>{ project.name }</Link> }
                         </Line>
                     ))}
                     <Line number={++number} />
                     {me.contacts.map(contact => (
                         <Line key={ contact.name } tag="contact" number={ ++number }>
-                            <Link className="text-[#35BAEB]" href={ contact.url }>{ contact.name }</Link>
+                            <Link className="text-[#35BAEB]" href={ contact.link }>{ contact.name }</Link>
                         </Line>
                     ))}
                 </React.Fragment>
