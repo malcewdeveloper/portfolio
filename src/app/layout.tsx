f53e107f2a6b8bac85cac74ef0f651a9080c33ec
Maksim <malcewdeveloper@yandex.ru>
2024-03-18 23:18:16 +0300

    refactor: adds responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={ golos.className }>{ children }</body>
+      <body className={ `${golos.className} min-w-80` }>{ children }</body>
     </html>
   );
 }
